6ba011023a8cbe447f210db6f519cc63e9c99d75
Talyson Vitorino <talysondev@gmail.com>
2024-08-08 13:58:58 -0300

    Print movimentos possiveis

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,6 +21,9 @@ public class Program {
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
+                boolean[][] possibleMoves = chessmatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessmatch.getPieces(),possibleMoves);
 
                 System.out.println();
                 System.out.print("Target: ");
